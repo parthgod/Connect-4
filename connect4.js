c876e088a23e1e3a41dc14ba727dce9d91c618a0
parthgod <118754435+parthgod@users.noreply.github.com>
2022-11-22 00:25:23 +0530

    Add files via upload

--- connect4.js
+++ connect4.js
@@ -1,20 +1,24 @@
 let slots=document.querySelectorAll('.slots');
 let i=0;
-let stk=[];
 let wh=window.getComputedStyle(document.getElementById(0)).getPropertyValue('background-color');
-let text=document.getElementById("win");
 
+let text=document.getElementById("win");
 document.getElementById("writing").innerHTML += "🔴"+localStorage.getItem("play1")+"   VS   "+localStorage.getItem("play2")+"🟡"; 
 let ele=document.getElementById('writing').innerHTML.replace("VS","<span style='color:green'>VS</span>");
 document.getElementById("writing").innerHTML=ele;
+
+let stk=[];
 for(let j=0;j<7;j++)
 stk.push(new stack(6));
 let winner=0;
 let color="red";
+
 slots.forEach((slots,i)=>
 {
     slots.addEventListener('click',function handleClick()
     {
+        if(winner==1)
+        return;
         st=stk[i%7];
         let flag=st.push(color,i);
         let val=(i%7)+42-7*stk[i%7].stacktop;
@@ -26,55 +30,33 @@ slots.forEach((slots,i)=>
             color="red";
         }
         const q=new queue();
-        let count=0;
         if(val%7!=6 && val>6)
         {
             if(val-6>=0)
-            {
-                q.addqueue(val-6);
-                count++;
-            }
+            q.addqueue(val-6);
         }
         if(val+1<49 && i%7!=6)
-        {
-            q.addqueue(val+1);
-            count++;
-        }
+        q.addqueue(val+1);
         if(val%7!=6 && val<42)
         {
             if(val+8<49)
-            {
-                q.addqueue(val+8);
-                count++;
-            }
+            q.addqueue(val+8);
         }
         if(val+7<49 && i<42)
-        {
-            q.addqueue(val+7);
-            count++;
-        }
+        q.addqueue(val+7);
         if(val%7!=0 && val<42)
         {
             if(val+6<49)
-            {
-                q.addqueue(val+6);
-                count++;
-            }
+            q.addqueue(val+6);
         }
         if(val-1>=0 && i%7!=0)
-        {
-            q.addqueue(val-1);
-            count++;
-        }
+        q.addqueue(val-1);
         if(val%7!=0 && val>6)
         {
             if(val-8>=0)
-            {
-                q.addqueue(val-8);
-                count++;
-            }
+            q.addqueue(val-8);
         }
-        for(var j=0;j<count;j++)
+        for(var j=0;j<q.arr.length;j++)
         {
             let c1=0;
             let c2=0;
